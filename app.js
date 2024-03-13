7f1408367c968edcd8f3b86b2dac16faf820b537
Prajwal Hebbar A S <chaotic-thoughts@Prajwals-MacBook-Pro.local>
2024-03-13 20:55:08 +0530

    Event Binding and counter app

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 const vue = Vue.createApp({
   data: function () {
     return {
-      goalA: "Learn Vue!",
-      goalB: "Learn React!",
-      htmlContent: "<h2 style='color: white'>Test Header</h2>",
-      vueLink: "https://vuejs.org/",
+      count: 0,
     };
-  },
-  methods: {
-    randomGoal: function () {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return this.goalA;
-      } else {
-        return this.goalB;
-      }
-    },
   },
 });
 
